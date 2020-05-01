4cb5b694dd9a354b034d5873d34b57d09f93133e
Sławek Chabowski <slawomir.chabowski@gmail.com>
2020-05-01 15:28:14 +0200

    Updated error component

--- src/views/Error/Error.jsx
+++ src/views/Error/Error.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './Error.scss';
+import { Container, Card, CardBody, CardTitle, Button } from 'shards-react';
+import { Link } from 'react-router-dom';
 
 function Error() {
-  return <div>Error 404 page not found</div>;
-};
+  return <Container fluid>
+    <Card>
+
+      <CardBody>
+        <CardTitle>Page not found </CardTitle>
+        <p>Could not find anything at {window.location.href}.</p>
+
+        <div className="btn-container">
+          <Link to="/">
+            <Button>Home</Button>
+          </Link>
+
+          <Button theme="light" onClick={() => window.history.back()}>Go back</Button>
+        </div>
+      </CardBody>
+    </Card>
+  </Container>;
+}
 
 export default Error;
