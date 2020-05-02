2e74419cdbd6e9b7b8e49a0b0b394b14e1e1a3ac
Sławek Chabowski <slawomir.chabowski@gmail.com>
2020-05-02 11:06:48 +0200

    Updates

---
+++ precache-manifest.eb4d758c78e5bf13bb32ce7ecb3423a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75785443ff146c0769eec6020f756f41",
+    "url": "/wallpapper-aggregator/index.html"
+  },
+  {
+    "revision": "399ff012db3a5ad6c049",
+    "url": "/wallpapper-aggregator/static/css/2.2ffde8b7.chunk.css"
+  },
+  {
+    "revision": "baa8ab9623425372729f",
+    "url": "/wallpapper-aggregator/static/css/main.54a5d39e.chunk.css"
+  },
+  {
+    "revision": "399ff012db3a5ad6c049",
+    "url": "/wallpapper-aggregator/static/js/2.bc256e32.chunk.js"
+  },
+  {
+    "revision": "bc1a73d08d8433691746793d8481dea3",
+    "url": "/wallpapper-aggregator/static/js/2.bc256e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa8ab9623425372729f",
+    "url": "/wallpapper-aggregator/static/js/main.0f42751e.chunk.js"
+  },
+  {
+    "revision": "213b695ce6a900db66d9",
+    "url": "/wallpapper-aggregator/static/js/runtime-main.993d03c0.js"
+  }
+]);
