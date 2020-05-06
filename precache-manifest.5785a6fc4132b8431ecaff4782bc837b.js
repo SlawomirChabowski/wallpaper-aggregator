8943af1b570bb157d209c6282390e29cefa2e806
Sławek Chabowski <slawomir.chabowski@gmail.com>
2020-05-06 20:09:12 +0200

    Updates

---
+++ precache-manifest.5785a6fc4132b8431ecaff4782bc837b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9365c5d99de2b8a7cd934ecd81db853f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ae0700ff91d2cf41366",
+    "url": "./static/css/2.2ffde8b7.chunk.css"
+  },
+  {
+    "revision": "bb41b8209b422757ca50",
+    "url": "./static/css/main.1e87e03d.chunk.css"
+  },
+  {
+    "revision": "4ae0700ff91d2cf41366",
+    "url": "./static/js/2.d34bccfa.chunk.js"
+  },
+  {
+    "revision": "bc1a73d08d8433691746793d8481dea3",
+    "url": "./static/js/2.d34bccfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb41b8209b422757ca50",
+    "url": "./static/js/main.518bc6e2.chunk.js"
+  },
+  {
+    "revision": "86a06ef1f78627b34113",
+    "url": "./static/js/runtime-main.ca4b0d2d.js"
+  }
+]);
